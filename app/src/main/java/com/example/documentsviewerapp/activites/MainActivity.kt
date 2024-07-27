308d97ec48e40ddb2d61a3f20fc514f041301ef1
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2024-07-27 14:54:20 +0400

    Add ask permissions

--- app/src/main/java/com/example/documentsviewerapp/activites/MainActivity.kt
+++ app/src/main/java/com/example/documentsviewerapp/activites/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.documentsviewerapp.activites
 
 import android.os.Bundle
-import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.documentsviewerapp.R
--- app/src/main/java/com/example/documentsviewerapp/fragments/MainPageFragment.kt
+++ app/src/main/java/com/example/documentsviewerapp/fragments/MainPageFragment.kt
@@ -2,12 +2,20 @@ package com.example.documentsviewerapp.fragments
 
 import android.os.Bundle
 import android.app.Fragment
+import android.content.pm.PackageManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.app.ActivityCompat
 import com.example.documentsviewerapp.R
 class MainPageFragment : Fragment() {
+
+    val permissions = arrayOf(
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     private lateinit var showBtn: Button
     override fun onCreateView(
@@ -17,8 +25,31 @@ class MainPageFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_main_page, container, false)
 
         initView(view)
+        setClickListeners()
 
         return view
+    }
+
+    private fun askPermissions() {
+        if (ActivityCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(context,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                activity,
+                permissions,
+                PackageManager.PERMISSION_GRANTED
+            )
+        } else {
+            Log.e("DB", "PERMISSION GRANTED");
+        }
+    }
+
+    private fun setClickListeners() {
+        showBtn.setOnClickListener {
+            askPermissions()
+        }
     }
 
     private fun initView(view: View) {
